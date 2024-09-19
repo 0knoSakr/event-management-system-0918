f36306a48ad3b0a70f8f0641380468eee05c2cda
0knoSakr <sakura.okano@gmail.com>
2024-09-19 11:21:32 +0900

    スペルミスの修正

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
 // /src/App.js
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
+// import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import CreateEventPage from './pages/CreateEventPage';
 import EventListPage from './pages/EventListPage';
-import Navber from './components/Navbar';
+import Navbar from './components/Navbar';
 
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
-        <Navber />
+        <Navbar />
         <Routes>
             <Route path="/" element={ <LoginPage />} />
             <Route path="/signup" element={ <SignupPage/>}/>
             <Route path="/create-event" element={ <CreateEventPage/> } />
             <Route path="/event-list" element={ <EventListPage/> } />
-          </Routes>
+        </Routes>
       </BrowserRouter>
     </>
 
